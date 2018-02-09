dbe37fe453b7689b8d27284e5e5c20a00f1902fd
Humberto Boone <h.boone@outlook.com>
2018-02-09 16:44:10 -0700

    Update to append div group

--- webroot/js/new_item.js
+++ webroot/js/new_item.js
@@ -1,9 +1,11 @@
 t_groups = 0;
 $(document).ready(function () {
-  $('btnNewGroup').click(function(){
-
+  $('#btnNewGroup').click(function(){
+    alert('hola');
+    $('#options_wrapper').append('<div id="group' + t_groups + '"></div>');
   });
 });
 function newGroup(){
-    $('options_wraper').append();
+    $('#options_wraper').append('<div id="group'+t_groups+'"></div>');
+    t_groups++;
 }
