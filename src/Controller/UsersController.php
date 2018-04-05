169d19f48888320bb355fc1022b4e55d6a2dc9eb
urrutia32 <joselurrutiar@gmail.com>
2018-04-05 17:29:01 -0600

    Fix redirect in users login

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -21,7 +21,7 @@ class UsersController extends AppController
             $user = $this->Auth->identify();
             if ($user) {
                 $this->Auth->setUser($user);
-                if($params['redirect'] == 'cart'){
+                if(isset($params['redirect']) && $params['redirect'] == 'cart'){
                     return $this->redirect(['controller' => 'Orders', 'action' => 'shipping']);
                 }
                 return $this->redirect($this->Auth->redirectUrl());
